0a5a48fd9c6dcfb4478432da536e6e1fa956daf8
Lucas Boinet <42383760+lucasboinet@users.noreply.github.com>
2022-03-04 14:45:05 +0100

    created few components

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './Homepage.scss';
 import FloatingCard from '../../shared/components/FloatingCard/FloatingCard';
+import Picture from '../../modules/Player/Avatar/atoms/Picture/Picture';
 
 export default function Homepage(){
     return (
         <div className='homepage-container'>
             <FloatingCard>
                 <h1>Jouer</h1>
+                <div className='avatar-container'>
+                    <Picture src="images/player.jpg" size="130" />
+                    <div className='avatar-select'>
+                        <button>Importer une photo</button>
+                        <div className='default-avatar'>
+                            <Picture src="images/player.jpg" size="50" />
+                            <Picture src="images/player.jpg" size="50" />
+                            <Picture src="images/player.jpg" size="50" />
+                        </div>
+                    </div>
+                </div>
             </FloatingCard>
         </div>
     )

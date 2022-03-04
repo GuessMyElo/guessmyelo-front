20b4ecb878e25a83a9e52a56d345f9969feac267
KizutoFR <yanisbenamor38@gmail.com>
2022-03-04 17:25:25 +0100

    SidePanel test

--- src/modules/Gameplay/atomes/VotingButton.js
+++ src/modules/Gameplay/atomes/VotingButton.js
@@ -3,7 +3,9 @@ import './VotingButton.scss';
 
 export default function VotingButton({src, id, alt}){
     return (
-        <button id={id} className='votingbutton-container'>
+        <button id={id} className='votingbutton-container' onClick={() =>{
+            console.log(id);
+        }}>
             <img src={src} alt={alt}/>
         </button>
     )
---
+++ src/modules/Gameplay/shared/components/SidePanel/SidePanel.js
@@ -0,0 +1,9 @@
+import './SidePanel.scss';
+
+export default function SidePanel({children}) {
+    return (
+        <div className="sidepanel-container">
+            {children}
+        </div>
+    )
+}

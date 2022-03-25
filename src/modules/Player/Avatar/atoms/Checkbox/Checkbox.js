8a7b2832ccc58607da5baac821515fca4265cd3b
KizutoFR <yanisbenamor38@gmail.com>
2022-03-25 17:55:58 +0100

    Avancement du front de la page de jeu

--- src/modules/Player/Avatar/atoms/Checkbox/Checkbox.js
+++ src/modules/Player/Avatar/atoms/Checkbox/Checkbox.js
@@ -2,11 +2,12 @@ import './Checkbox.scss';
 import { useEffect } from 'react';
 
 export default function Checkbox({status, size}) {
-
+    var test = false;
     const _playerStatus = () => {
         console.log("playerstatus");
         if (status === "done") {
             console.log("done");
+            test=true
             return "#2ACE5B";
         } else {
             console.log("waiting");
@@ -18,6 +19,16 @@ export default function Checkbox({status, size}) {
     },[status]);
 
     return (
-        <div className={'checkbox'} style={{width: size + 'px', height: size + 'px', backgroundColor: status === "done" ? "#2ACE5B" : "#6953C3" }}></div>
+        <div className={'checkbox'} style={{width: size + 'px', height: size + 'px', backgroundColor: status === "done" ? "#2ACE5B" : "#6953C3" }}>
+            {test ? (
+                //TODO Ajouté svg "V" : validated / checked
+                <p></p>
+            )
+            :(
+                //TODO Ajouté "..." animé (gif ou style "est en train d ecrire")
+                <p></p>
+            )
+            }
+        </div>
     )
 }

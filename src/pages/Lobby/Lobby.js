c60fccfb62e1118077e65cea077f59e8cfa31617
KizutoFR <yanisbenamor38@gmail.com>
2022-05-06 17:44:44 +0200

    Lobby page

--- src/pages/Lobby/Lobby.js
+++ src/pages/Lobby/Lobby.js
@@ -14,6 +14,9 @@ import InputField from 'shared/components/InputField/InputField';
 export default function Lobby(){
 
     const [difficulty, setDifficulty] = useState("");
+    const [participant, setParticipant] = useState("3");
+    const [nbrVideo, setNbrVideo] = useState("5");
+    const [nbrLoop, setNbrLoop] = useState("2");
     const difficultyOptions = ["facile","moyen","hard","extreme"]
 
     return (
@@ -22,9 +25,15 @@ export default function Lobby(){
                 <FloatingCard>
                     <h1>Paramètre</h1>
                     <form>
-                        
-                        <InputField type={"number"} placeholder="Code de la partie" />
-                        <Select options={difficultyOptions.map((value) => ({text : Capitalize(value), value }))} value={difficulty} onChange={(e) => setDifficulty(e.target.value)} backgroundcolor={"#fff"} textcolor={"#000"} />
+                        <label for="participant">Nombre de participants (3-20):</label>
+                        <InputField type={"number"} placeholder="Nombre de participant" id="participant" min="3" max="20" backgroundcolor="#fff" textcolor="#000" value={participant} onChange={(e) => setParticipant(e.target.value)}/>
+                        <label for="nbrVideo">Nombre de participants (3-10):</label>
+                        <InputField type={"number"} placeholder="Nombre de vidéo" id="nbrVideo" min="3" max="10" backgroundcolor="#fff" textcolor="#000" value={nbrVideo} onChange={(e) => setNbrVideo(e.target.value)}/>
+                        <label for="difficulty">Difficulté :</label>
+                        <Select id="difficulty" options={difficultyOptions.map((value) => ({text : Capitalize(value), value }))} value={difficulty} onChange={(e) => setDifficulty(e.target.value)} backgroundcolor={"#fff"} textcolor={"#000"} />
+                        <label for="nbrLoop">Nombre de passage de la vidéo (1-5):</label>
+                        <InputField type={"number"} placeholder="Nombre de passage de la vidéo " id="nbrLoop" min="1" max="5" backgroundcolor="#fff" textcolor="#000" value={nbrLoop} onChange={(e) => setNbrLoop(e.target.value)}/>
+
                         <Link to="/game">
                             <Button>Lancer le jeu</Button>
                         </Link>
--- src/shared/components/InputField/InputField.js
+++ src/shared/components/InputField/InputField.js
@@ -1,5 +1,5 @@
 import './InputField.scss';
 
-export default function InputField({placeholder, type, inputRef, ...rest}) {
-    return <input type={type || "text"} placeholder={placeholder} ref={inputRef} {...rest}/>
+export default function InputField({placeholder, type, inputRef,backgroundcolor,textcolor, ...rest}) {
+    return <input type={type || "text"} placeholder={placeholder} ref={inputRef} style={{ backgroundColor : backgroundcolor ?? "#1A213F", color : textcolor ?? "#7D8091"}}  {...rest}/>
 }

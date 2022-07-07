eff0ae78b0c8fb0b813e3c7b8873e4562c689405
KizutoFR <yanisbenamor38@gmail.com>
2022-07-07 11:16:28 +0200

    fixed css

--- src/modules/Gameplay/atoms/VotingButton/VotingButton.js
+++ src/modules/Gameplay/atoms/VotingButton/VotingButton.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './VotingButton.scss';
 
-export default function VotingButton({src, id, alt,handleVotingResponse}){
+export default function VotingButton({src, id, alt,handleVotingResponse,selected}){
     return (
-        <button title={id.toUpperCase()} id={id} className='votingbutton-container' onClick={() =>{
+        <button title={id.toUpperCase()} id={id} className={`votingbutton-container ${selected ? "active" : ""}`} onClick={(e) =>{
             handleVotingResponse(id)
         }}>
             <img src={src} alt={alt}/>
--- src/modules/Gameplay/molecules/VotingSection/VotingSection.js
+++ src/modules/Gameplay/molecules/VotingSection/VotingSection.js
@@ -2,18 +2,18 @@ import './VotingSection.scss';
 import '../../atoms/VotingButton/VotingButton.js';
 import VotingButton from '../../atoms/VotingButton/VotingButton.js';
 
-export default function VotingSection({handleVotingResponse}) {
+export default function VotingSection({handleVotingResponse,answer}) {
     return (
         <div className="votingsection-container">
-            <VotingButton handleVotingResponse={handleVotingResponse} id='iron' src="/images/rank_Iron.png" alt="Image du rang Iron ou Fer"/>
-            <VotingButton handleVotingResponse={handleVotingResponse} id='bronze' src="/images/rank_Bronze.png" alt="Image du rang Bronze"/>
-            <VotingButton handleVotingResponse={handleVotingResponse} id='silver' src="/images/rank_Silver.png" alt="Image du rang Silver ou Argent"/>
-            <VotingButton handleVotingResponse={handleVotingResponse} id='gold' src="/images/rank_Gold.png" alt="Image du rang Gold ou Or"/>
-            <VotingButton handleVotingResponse={handleVotingResponse} id='platinum' src="/images/rank_Platinum.png" alt="Image du rang Platinium ou Platine"/>
-            <VotingButton handleVotingResponse={handleVotingResponse} id='diamond' src="/images/rank_Diamond.png" alt="Image du rang Diamond ou Diamant"/>
-            <VotingButton handleVotingResponse={handleVotingResponse} id='master' src="/images/rank_Master.png" alt="Image du rang Master"/>
-            <VotingButton handleVotingResponse={handleVotingResponse} id='grandmaster' src="/images/rank_Grandmaster.png" alt="Image du rang Grandmaster"/>   
-            <VotingButton handleVotingResponse={handleVotingResponse} id='challenger' src="/images/rank_Challenger.png" alt="Image du rang Challenger"/> 
+            <VotingButton selected={answer==='iron'} handleVotingResponse={handleVotingResponse} id='iron' src="/images/rank_Iron.png" alt="Image du rang Iron ou Fer"/>
+            <VotingButton selected={answer==='bronze'} handleVotingResponse={handleVotingResponse} id='bronze' src="/images/rank_Bronze.png" alt="Image du rang Bronze"/>
+            <VotingButton selected={answer==='silver'} handleVotingResponse={handleVotingResponse} id='silver' src="/images/rank_Silver.png" alt="Image du rang Silver ou Argent"/>
+            <VotingButton selected={answer==='gold'} handleVotingResponse={handleVotingResponse} id='gold' src="/images/rank_Gold.png" alt="Image du rang Gold ou Or"/>
+            <VotingButton selected={answer==='platinum'} handleVotingResponse={handleVotingResponse} id='platinum' src="/images/rank_Platinum.png" alt="Image du rang Platinium ou Platine"/>
+            <VotingButton selected={answer==='diamond'} handleVotingResponse={handleVotingResponse} id='diamond' src="/images/rank_Diamond.png" alt="Image du rang Diamond ou Diamant"/>
+            <VotingButton selected={answer==='master'} handleVotingResponse={handleVotingResponse} id='master' src="/images/rank_Master.png" alt="Image du rang Master"/>
+            <VotingButton selected={answer==='grandmaster'} handleVotingResponse={handleVotingResponse} id='grandmaster' src="/images/rank_Grandmaster.png" alt="Image du rang Grandmaster"/>   
+            <VotingButton selected={answer==='challenger'} handleVotingResponse={handleVotingResponse} id='challenger' src="/images/rank_Challenger.png" alt="Image du rang Challenger"/> 
         </div>
     )
 }
